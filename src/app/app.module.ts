89d35b0cd0642d3035ea43cb6705af3ea180aa1d
clement.escande@3wa.io <clement.escande@3wa.io>
2021-09-14 15:01:28 +0200

    v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonPremierComponentComponent } from './mon-premier-component/mon-premier-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr');
+
+import {AppareilService} from "./services/appareil.service";
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './services/user.service';
+import { NewUserComponent } from './new-user/new-user.component';
+
+const appRoutes: Routes = [
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AuthComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'new-user', component: NewUserComponent },
+  { path: 'not-found', component: FourOhFourComponent},
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponentComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent,
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AppareilService,
+    AuthService,
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
